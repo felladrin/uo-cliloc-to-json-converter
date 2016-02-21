ac3417bd2d362a4bba26ba52ceaabeda003aba65
Victor Nogueira <felladrin@gmail.com>
2016-02-21 14:55:04 -0300

    Now it displays progress messages on console while converting files.

--- convert_cliloc_to_json.php
+++ convert_cliloc_to_json.php
@@ -5,34 +5,30 @@ $inputClilocs = [];
 $directoryItarator = new DirectoryIterator('input');
 
 foreach ($directoryItarator as $fileInfo)
-{
     if (strpos(strtolower($fileInfo->getFilename()), 'cliloc') !== false)
-    {
         array_push($inputClilocs, ['filePath' => $fileInfo->getPathname(), 'language' => strtoupper($fileInfo->getExtension())]);
-    }
-}
 
 $allLanguages = [];
 
 foreach ($inputClilocs as $cliloc)
-{
     array_push($allLanguages, $cliloc['language']);
-}
 
 $output = [];
 
 foreach ($inputClilocs as $cliloc)
 {
+    echo "Reading {$cliloc['filePath']}... ";
+
     $handle = fopen($cliloc['filePath'], 'rb');
+
     fseek($handle, 6);
+
     while (!feof($handle))
     {
         $numberBytes = fread($handle, 4);
 
         if (empty($numberBytes))
-        {
             break;
-        }
 
         fread($handle, 1);
         $lengthBytes = fread($handle, 2);
@@ -45,19 +41,20 @@ foreach ($inputClilocs as $cliloc)
         foreach ($allLanguages as $language)
         {
             if (empty($output[$number]))
-            {
                 $output[$number] = [];
-            }
 
             if (empty($output[$number][$language]))
-            {
                 $output[$number][$language] = '';
-            }
         }
 
         $output[$number][$cliloc['language']] = $text;
     }
+
     fclose($handle);
+
+    echo 'OK!' . PHP_EOL;
 }
 
-file_put_contents('json/Cliloc.json', json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+$saved = file_put_contents('json/Cliloc.json', json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+echo ($saved === false) ? 'Error! Cliloc.json could not be saved.' : 'Done! Cliloc.json has been saved in json folder. You can open and edit it using any text editor.';
--- convert_json_to_cliloc.php
+++ convert_json_to_cliloc.php
@@ -5,12 +5,8 @@ $inputClilocs = [];
 $directoryItarator = new DirectoryIterator('input');
 
 foreach ($directoryItarator as $fileInfo)
-{
     if (strpos(strtolower($fileInfo->getFilename()), 'cliloc') !== false)
-    {
         array_push($inputClilocs, ['filePath' => $fileInfo->getPathname(), 'language' => strtoupper($fileInfo->getExtension())]);
-    }
-}
 
 $allLanguages = [];
 
@@ -21,18 +17,19 @@ foreach ($inputClilocs as $cliloc)
     array_push($allLanguages, $cliloc['language']);
 
     if ($cliloc['language'] == "ENU")
-    {
         $useClilocEnuAsFallback = true;
-    }
 }
 
 foreach ($inputClilocs as $cliloc)
 {
     $outputFilePath = str_replace('input', 'output', $cliloc['filePath']);
 
+    echo "Writing {$outputFilePath}... ";
+
     $json = json_decode(file_get_contents('json/Cliloc.json'), true);
 
     $fp = fopen($outputFilePath, 'wb');
+
     fwrite($fp, pack('C*', 2, 0, 0, 0, 1, 0));
 
     foreach ($json as $number => $languages)
@@ -40,9 +37,7 @@ foreach ($inputClilocs as $cliloc)
         $text = $languages[$cliloc['language']];
 
         if (empty($text) && $useClilocEnuAsFallback)
-        {
             $text = $languages['ENU'];
-        }
 
         fwrite($fp, pack('V', $number));
         fwrite($fp, pack('C', 0));
@@ -51,4 +46,8 @@ foreach ($inputClilocs as $cliloc)
     }
 
     fclose($fp);
+
+    echo "OK!" . PHP_EOL;
 }
+
+echo "Done! Now copy these clilocs to UO folder, overwriting the old ones.";
